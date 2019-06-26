34aae8249b9384768d77903690c6f702c4628cc0
armandomtf <armandomoraistavares@gmail.com>
2019-06-26 19:34:21 -0300

    atualizaçoes no servlet e no hibernateutil e nas classes

--- horabolasnb/src/main/java/com/br/horabolas/util/HibernateUtil.java
+++ horabolasnb/src/main/java/com/br/horabolas/util/HibernateUtil.java
@@ -5,54 +5,39 @@
  */
 package com.br.horabolas.util;
 
-import com.br.horabolas.Usuario;
-import org.hibernate.HibernateException;
+import entidades.Usuario;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.cfg.AnnotationConfiguration;
-import org.hibernate.cfg.SettingsFactory;
 
 public class HibernateUtil {
 
-    private static final SessionFactory concreteSessionFactory;
+//    private static final SessionFactory sessionFactory;
+//    static {
+//        try {
+//            sessionFactory = new AnnotationConfiguration()
+//                    .configure().buildSessionFactory();
+//        } catch (Throwable ex) {
+//            // Log exception!
+//            throw new ExceptionInInitializerError(ex);
+//        }
+//    }
+//
+//    public static Session getSession()
+//            throws HibernateException {
+//        return sessionFactory.openSession();
+//    }
+    
+    private static SessionFactory factory;
 
     static {
-        try {
-            concreteSessionFactory = new AnnotationConfiguration()
-                    .configure()
-                    .addAnnotatedClass(Usuario.class)
-                    .buildSessionFactory();
-        } catch (Throwable ex) {
-            throw new ExceptionInInitializerError(ex);
-        }
+        AnnotationConfiguration cfg = new AnnotationConfiguration();
+        cfg.configure();
+        cfg.addAnnotatedClass(Usuario.class);
+        factory = cfg.buildSessionFactory();
     }
 
-    public static Session getSession()
-            throws HibernateException {
-        return concreteSessionFactory.openSession();
-    }
-
-    public static void main(String... args) {
-        Session session = getSession();
-        Transaction tr = session.beginTransaction();
-        Usuario user;
-        user = (Usuario) session.get(Usuario.class, new Integer(1));
-        if (user == null) {
-            System.out.println("Usuário não encontrado.");
-        } else {
-            System.out.println("Usuario: "+user.getUsuario());
-        }
-        
-        Usuario usuario = new Usuario();
-        usuario.setEmail("jogador@hb.com");
-        usuario.setUsuario("jogador");
-        usuario.setCpf("00000000000");
-        usuario.setSenha("123");
-        
-        session.save(usuario);
-        tr.commit();
-        
-        session.close();
+    public static Session getSession() {
+        return factory.openSession();
     }
 }
