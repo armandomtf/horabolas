6cb5ca4a2689ef5adc2a414ef70725f7e70dde38
armandomtf <armandomoraistavares@gmail.com>
2019-09-20 22:51:56 -0300

    dbatt

--- hbbackup.sql
+++ hbbackup.sql
@@ -2,16 +2,18 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 9.5.7
--- Dumped by pg_dump version 9.5.7
+-- Dumped from database version 9.5.19
+-- Dumped by pg_dump version 9.5.19
 
--- Started on 2019-09-18 12:05:13 BRT
+-- Started on 2019-09-20 22:23:23 BRT
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
+SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
@@ -24,7 +26,7 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- TOC entry 2155 (class 0 OID 0)
+-- TOC entry 2158 (class 0 OID 0)
 -- Dependencies: 1
 -- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
 --
@@ -32,14 +34,12 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 
 
-SET search_path = public, pg_catalog;
-
 --
--- TOC entry 181 (class 1259 OID 41635)
+-- TOC entry 181 (class 1259 OID 16385)
 -- Name: sq_usuario; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
-CREATE SEQUENCE sq_usuario
+CREATE SEQUENCE public.sq_usuario
     START WITH 0
     INCREMENT BY 1
     MINVALUE 0
@@ -47,14 +47,14 @@ CREATE SEQUENCE sq_usuario
     CACHE 1;
 
 
-ALTER TABLE sq_usuario OWNER TO postgres;
+ALTER TABLE public.sq_usuario OWNER TO postgres;
 
 --
--- TOC entry 184 (class 1259 OID 41650)
+-- TOC entry 182 (class 1259 OID 16387)
 -- Name: sq_usuarioadm; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
-CREATE SEQUENCE sq_usuarioadm
+CREATE SEQUENCE public.sq_usuarioadm
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -62,18 +62,18 @@ CREATE SEQUENCE sq_usuarioadm
     CACHE 1;
 
 
-ALTER TABLE sq_usuarioadm OWNER TO postgres;
+ALTER TABLE public.sq_usuarioadm OWNER TO postgres;
 
 SET default_tablespace = '';
 
 SET default_with_oids = false;
 
 --
--- TOC entry 182 (class 1259 OID 41637)
+-- TOC entry 183 (class 1259 OID 16389)
 -- Name: usuario; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE usuario (
+CREATE TABLE public.usuario (
     id integer NOT NULL,
     nome character varying,
     email character varying,
@@ -81,14 +81,14 @@ CREATE TABLE usuario (
 );
 
 
-ALTER TABLE usuario OWNER TO postgres;
+ALTER TABLE public.usuario OWNER TO postgres;
 
 --
--- TOC entry 183 (class 1259 OID 41645)
+-- TOC entry 184 (class 1259 OID 16395)
 -- Name: usuarioadm; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE usuarioadm (
+CREATE TABLE public.usuarioadm (
     senha character varying(20) NOT NULL,
     id integer NOT NULL,
     nome character varying(50) NOT NULL,
@@ -96,74 +96,77 @@ CREATE TABLE usuarioadm (
 );
 
 
-ALTER TABLE usuarioadm OWNER TO postgres;
+ALTER TABLE public.usuarioadm OWNER TO postgres;
 
 --
--- TOC entry 2156 (class 0 OID 0)
+-- TOC entry 2159 (class 0 OID 0)
 -- Dependencies: 181
 -- Name: sq_usuario; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('sq_usuario', 8, true);
+SELECT pg_catalog.setval('public.sq_usuario', 8, true);
 
 
 --
--- TOC entry 2157 (class 0 OID 0)
--- Dependencies: 184
+-- TOC entry 2160 (class 0 OID 0)
+-- Dependencies: 182
 -- Name: sq_usuarioadm; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('sq_usuarioadm', 1, false);
+SELECT pg_catalog.setval('public.sq_usuarioadm', 1, false);
 
 
 --
--- TOC entry 2145 (class 0 OID 41637)
--- Dependencies: 182
+-- TOC entry 2148 (class 0 OID 16389)
+-- Dependencies: 183
 -- Data for Name: usuario; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY usuario (id, nome, email, senha) FROM stdin;
-301	BRIAN MATADOR	brian@gmail.com	123
-302	toni mica	antoni@gmail.com	1234
-303	miguelzin fred	miguel@gmail.com	123
-350	TATA ZUANDO	talis@gmail.com	123
-351	PC	pc@gmail.com	123
-400	higuita	higuita@gmail.com	1234567
+COPY public.usuario (id, nome, email, senha) FROM stdin;
 \.
 
 
 --
--- TOC entry 2146 (class 0 OID 41645)
--- Dependencies: 183
+-- TOC entry 2149 (class 0 OID 16395)
+-- Dependencies: 184
 -- Data for Name: usuarioadm; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY usuarioadm (senha, id, nome, email) FROM stdin;
+COPY public.usuarioadm (senha, id, nome, email) FROM stdin;
 \.
 
 
 --
--- TOC entry 2027 (class 2606 OID 41644)
+-- TOC entry 2027 (class 2606 OID 16399)
 -- Name: pk_id; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY usuario
+ALTER TABLE ONLY public.usuario
     ADD CONSTRAINT pk_id PRIMARY KEY (id);
 
 
 --
--- TOC entry 2029 (class 2606 OID 41649)
+-- TOC entry 2031 (class 2606 OID 16401)
 -- Name: pk_idadm; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY usuarioadm
+ALTER TABLE ONLY public.usuarioadm
     ADD CONSTRAINT pk_idadm PRIMARY KEY (id);
 
 
 --
--- TOC entry 2154 (class 0 OID 0)
+-- TOC entry 2029 (class 2606 OID 16403)
+-- Name: unique_email; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.usuario
+    ADD CONSTRAINT unique_email UNIQUE (email);
+
+
+--
+-- TOC entry 2157 (class 0 OID 0)
 -- Dependencies: 7
--- Name: public; Type: ACL; Schema: -; Owner: postgres
+-- Name: SCHEMA public; Type: ACL; Schema: -; Owner: postgres
 --
 
 REVOKE ALL ON SCHEMA public FROM PUBLIC;
@@ -172,7 +175,7 @@ GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
--- Completed on 2019-09-18 12:05:13 BRT
+-- Completed on 2019-09-20 22:23:23 BRT
 
 --
 -- PostgreSQL database dump complete
